c1220f7d83c87ff05df6b906e009e9d40f46955f
Zuber <zuber00124@gmail.com>
2017-09-30 14:32:33 +0530

    logout through mutation

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import query from '../queries/CurrentUser';
+import mutation from '../mutations/Logout';
 
 class Header extends Component {
+  onLogoutClick() {
+
+  }
+
   renderButtons() {
     const { loading, user } = this.props.data;
 
     if (loading) { return <div />; }
 
-    if(user) {
-      return <div>Logout</div>;
-    }else {
+    if (user) {
+      return (
+        <li><a onClick={this.onLogoutClick.bind(this)}>Logout</a></li>
+      );
+    } else {
       return (
         <div>
           <li>
@@ -43,4 +50,6 @@ class Header extends Component {
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(
+  graphql(query)(Header)
+); 
