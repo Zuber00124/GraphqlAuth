75960f53a77ef29f48789d5b5315e4faabf5ccf0
Zuber <zuber00124@gmail.com>
2017-09-30 14:54:17 +0530

    LoginForm Component

---
+++ client/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div>
+        login
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
